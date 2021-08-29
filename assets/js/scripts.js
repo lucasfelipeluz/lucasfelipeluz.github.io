7f16e0478c9ca347ed10150ae214b0b6036974bc
Lucas Luz <lucasfelipe281199@gmail.com>
2021-08-29 17:38:43 -0300

    resolvido bug no nav bar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,13 @@
 /* eslint-disable import/extensions */
 import NavBar from './navBar.js';
 import Habilidades from './habilidades.js';
+import ScrollNavBar from './navBarScroll.js';
 
 const navBarClass = new NavBar('.nav-button', '.menu-mobile', '[data-target]', '.sair-menu', '.burger-button');
 navBarClass.iniciar();
 
 const habilidades = new Habilidades('li[data-tag]', '.conteudo-tag');
 habilidades.iniciar();
+
+const navBarScroll = new ScrollNavBar('[data-target]');
+navBarScroll.iniciar();
