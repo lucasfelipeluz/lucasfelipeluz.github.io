7812b44b038487e309f1bbd3a82d9f4c9033346b
Lucas Luz <lucasfelipe281199@gmail.com>
2021-08-29 11:39:50 -0300

    habilidades refatorado + ajustes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,9 @@
 /* eslint-disable import/extensions */
 import NavBar from './navBar.js';
+import Habilidades from './habilidades.js';
 
 const navBarClass = new NavBar('.nav-button', '.menu-mobile', '[data-target]', '.sair-menu', '.burger-button');
 navBarClass.iniciar();
+
+const habilidades = new Habilidades('li[data-tag]', '.conteudo-tag');
+habilidades.iniciar();
