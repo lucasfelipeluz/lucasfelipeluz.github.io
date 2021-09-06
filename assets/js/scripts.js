49a3797756713bcd8b00fbf35c2f4c79043f0cc4
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2021-09-06 01:46:55 -0300

    sistema assincrono adicionado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,7 @@
 import NavBar from './navBar.js';
 import Habilidades from './habilidades.js';
 import ScrollNavBar from './navBarScroll.js';
+import CarregarHabilidades from './CarregarHabilidades.js';
 
 const navBarClass = new NavBar('.nav-button', '.menu-mobile', '[data-target]', '.sair-menu', '.burger-button');
 navBarClass.iniciar();
@@ -11,3 +12,6 @@ habilidades.iniciar();
 
 const navBarScroll = new ScrollNavBar('[data-target]');
 navBarScroll.iniciar();
+
+const carregarHabilidades = new CarregarHabilidades();
+carregarHabilidades.iniciar();
